44450492e4803b77a044a8f663256194092dc757
Hussain Chinoy <ghchinoy@google.com>
2024-08-19 17:29:14 -0600

    doc: remove unecessary comments

--- main.py
+++ main.py
@@ -555,12 +555,6 @@ def app():
                                             ),
                                         )
                                 
-                                # Image commentary
-                                #with me.box(style=me.Style(flex_wrap="wrap", display="flex", #flex_direction="row", width="85%", padding=me.Padding.all(10))):
-                                #    me.icon("assistant")
-                                #    me.text("magazine editor")                                    
-                                #    me.text(state.image_commentary)
-                                
                                 # SynthID notice
                                 with me.box(style=me.Style(display="flex", flex_direction="row", align_items="center")):
                                     svg_icon_component(svg="""<svg data-icon-name="digitalWatermarkIcon" viewBox="0 0 24 24" width="24" height="24" fill="none" aria-hidden="true" sandboxuid="2"><path fill="#3367D6" d="M12 22c-.117 0-.233-.008-.35-.025-.1-.033-.2-.075-.3-.125-2.467-1.267-4.308-2.833-5.525-4.7C4.608 15.267 4 12.983 4 10.3V6.2c0-.433.117-.825.35-1.175.25-.35.575-.592.975-.725l6-2.15a7.7 7.7 0 00.325-.1c.117-.033.233-.05.35-.05.15 0 .375.05.675.15l6 2.15c.4.133.717.375.95.725.25.333.375.717.375 1.15V10.3c0 2.683-.625 4.967-1.875 6.85-1.233 1.883-3.067 3.45-5.5 4.7-.1.05-.2.092-.3.125-.1.017-.208.025-.325.025zm0-2.075c2.017-1.1 3.517-2.417 4.5-3.95 1-1.55 1.5-3.442 1.5-5.675V6.175l-6-2.15-6 2.15V10.3c0 2.233.492 4.125 1.475 5.675 1 1.55 2.508 2.867 4.525 3.95z" sandboxuid="2"></path><path fill="#3367D6" d="M12 16.275c0-.68-.127-1.314-.383-1.901a4.815 4.815 0 00-1.059-1.557 4.813 4.813 0 00-1.557-1.06 4.716 4.716 0 00-1.9-.382c.68 0 1.313-.128 1.9-.383a4.916 4.916 0 002.616-2.616A4.776 4.776 0 0012 6.475c0 .672.128 1.306.383 1.901a5.07 5.07 0 001.046 1.57 5.07 5.07 0 001.57 1.046 4.776 4.776 0 001.901.383c-.672 0-1.306.128-1.901.383a4.916 4.916 0 00-2.616 2.616A4.716 4.716 0 0012 16.275z" sandboxuid="2"></path></svg>""")
@@ -587,7 +581,7 @@ def app():
                                 padding=me.Padding.all(10))):
                                     me.icon("assistant")
                                     me.text("magazine editor", style=me.Style(font_weight=500))                                    
-                                    me.text(state.image_commentary, style=me.Style(padding=me.Padding.all(15)))
+                                    me.markdown(text=state.image_commentary, style=me.Style(padding=me.Padding.all(15)))
             
         footer()                             
 
