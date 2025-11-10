1ca01823d94bd5a7e824c2479da7c61976f0f1c9
G H Chinoy <ghchinoy@gmail.com>
2025-11-09 19:20:28 -0700

    Fix build and deploy scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 PROJECT_ID=$(gcloud config get project)
 REGION=us-central1
 USE_MEDIA_PROXY=false
-gcloud builds submit . --project=$PROJECT_ID --gcs-source-staging-dir=gs://run-resources-$PROJECT_ID-$REGION/services/creative-studio --region=$REGION --service-account=projects/$PROJECT_ID/serviceAccounts/builds-creative-studio@$PROJECT_ID.iam.gserviceaccount.com --set-env-var USE_MEDIA_PROXY=${USE_MEDIA_PROXY}
+gcloud builds submit . --project=$PROJECT_ID --gcs-source-staging-dir=gs://run-resources-$PROJECT_ID-$REGION/services/creative-studio --region=$REGION --service-account=projects/$PROJECT_ID/serviceAccounts/builds-creative-studio@$PROJECT_ID.iam.gserviceaccount.com --substitutions=_USE_MEDIA_PROXY=${USE_MEDIA_PROXY}
