66760d8fef73ba9dd282546b0ce29c1c621e9c9c
Hussain Chinoy <ghchinoy@google.com>
2025-12-07 11:42:51 -0700

    refactor: adjust c2pa svg visibility fixes #959

--- config/default.py
+++ config/default.py
@@ -53,7 +53,7 @@ class NavConfig(BaseModel):
 class Default:
     """Defaults class"""
 
-    VERSION: str = "1.3.10" # storyboarder fix
+    VERSION: str = "1.3.11" # c2pa visibility fix
     APP_ENV: str = os.environ.get("APP_ENV", "")
     API_BASE_URL: str = os.environ.get(
         "API_BASE_URL", f"http://localhost:{os.environ.get('PORT', '8080')}"
